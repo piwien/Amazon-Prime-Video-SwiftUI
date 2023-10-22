219999c11547841d8772c59497147a70ed38f033
piwien <berkayyasar01@gmail.com>
2023-10-22 12:30:13 +0300

    Initial Commit

---
+++ Amazon-Prime-Video-SwiftUI/Amazon_Prime_Video_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amazon_Prime_Video_SwiftUIApp.swift
+//  Amazon-Prime-Video-SwiftUI
+//
+//  Created by Berkay Yaşar on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Amazon_Prime_Video_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
