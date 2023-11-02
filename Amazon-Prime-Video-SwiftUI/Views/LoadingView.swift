5368b10e5bab17904a585e581b8f83b915c71cf0
piwien <berkayyasar01@gmail.com>
2023-11-02 15:38:58 +0300

    LoadingView designed

---
+++ Amazon-Prime-Video-SwiftUI/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Amazon-Prime-Video-SwiftUI
+//
+//  Created by Berkay Yaşar on 2.11.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
